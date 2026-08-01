97cf53743230f02e4221e20bd5acafa4c122eef3
AttilaSzeremi <Attila.SZEREMI@student.howest.be>
2026-08-01 23:46:20 +0200

    Created a TextureManager Class
    Created a texture manager to handle the textures and texts and to make referencing them easier by using enumerators.

---
+++ DAE14_Szeremi_Attila/VampireSurvivors/TextureManager.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+class Texture;
+class TextureManager final
+{
+public:
+	enum class textureId
+	{
+		startScreenBG = 0,
+		button_blue
+	};
+	enum class textId
+	{
+
+	};
+
+	TextureManager();
+	~TextureManager();
+
+	Texture& GetTexture(textureId texture);
+	Texture& GetTexts(textId text);
+private:
+	std::vector<Texture*> m_pTextures;
+	std::vector<Texture*> m_pTexts;
+
+
+	void LoadTextures();
+	void LoadTexts();
+};
+
